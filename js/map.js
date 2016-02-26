b248e5653b1d3b7066736dfd396ad6f3fc0a15de
podiujop <podebradyujop@yandex.ru>
2016-02-26 02:09:14 +0100

    Go

--- js/map.js
+++ js/map.js
@@ -1,27 +1,31 @@
-ymaps.ready(init);
-var myMap;
+$(document).ready(function(){
+	console.log("dom loaded");
+	ymaps.ready(init);
+	var myMap;
 
-function init(){
-	myMap = new ymaps.Map("map", {
-		center: [28.39722, 36.57889],
-		zoom: 2,
-		controls: ['zoomControl', 'typeSelector',  'fullscreenControl']
-		},
+	function init(){
+		myMap = new ymaps.Map("map", {
+			center: [28.39722, 36.57889],
+			zoom: 2,
+			controls: ['zoomControl', 'typeSelector',  'fullscreenControl']
+			},
 
-	objectManager = new ymaps.ObjectManager({
-			clusterize: true,
-			gridSize: 32
-		})
-	);
+		objectManager = new ymaps.ObjectManager({
+				clusterize: true,
+				gridSize: 32
+			})
+		);
 
-	objectManager.objects.options.set('preset', 'islands#darkOrangeIcon');
-	objectManager.clusters.options.set('preset', 'islands#darkOrangeClusterIcons');
-	myMap.geoObjects.add(objectManager);
+		objectManager.objects.options.set('preset', 'islands#darkOrangeIcon');
+		objectManager.clusters.options.set('preset', 'islands#darkOrangeClusterIcons');
+		myMap.geoObjects.add(objectManager);
 
-	$.ajax({
-		url: "magic/data.json"
-	}).done(function(data) {
-		objectManager.add(data);
-	});
+		$.ajax({
+			url: "magic/data.json"
+		}).done(function(data) {
+			objectManager.add(data);
+		});
 
-}
+	}
+	console.log("map loaded");
+});
